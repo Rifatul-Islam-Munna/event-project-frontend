9ce28399e8b816ccb6394d1af19fc36861ca4e52
Rifatul-Islam-Munna <islamrifat117@gmail.com>
2025-09-03 14:18:00 +0600

    feat: Update SiteFooter to dynamically display header title from API

--- src/components/custom/common/site-footer.tsx
+++ src/components/custom/common/site-footer.tsx
@@ -1,11 +1,18 @@
+import { getHeader } from "@/actions/fetch-action";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
 export function SiteFooter() {
+  const { data, isPending, refetch } = useQuery({
+    queryKey: ["header"],
+    queryFn: () => getHeader(),
+    gcTime: 1000 * 60 * 60,
+  });
   return (
     <footer className="w-full py-6 border-t bg-background">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row md:px-6">
         <p className="text-sm text-muted-foreground">
-          &copy; {new Date().getFullYear()} Digital Seating. All rights
+          &copy; {new Date().getFullYear()} {data?.data?.title}. All rights
           reserved.
         </p>
         <nav className="flex gap-4 sm:gap-6">
