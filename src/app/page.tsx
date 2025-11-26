69d34f025c1819928300f826f82f59151fb036dd
Rifatul-Islam-Munna <islamrifat117@gmail.com>
2025-11-26 12:49:46 +0600

    removed slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col  ">
       <main className="flex-1">
-        <HeroSlider />
+        {/*  <HeroSlider /> */}
         <HeroSection />
         <FeaturesSection />
         <HowItWorksSection />
